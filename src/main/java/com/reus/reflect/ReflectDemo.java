4dff151739dc75f55f956576b3d4054ac7480bcb
liuxiaoqiang <liuxiaoqiang@wdai.com>
2018-02-27 16:19:30 +0800

    反射

--- src/main/java/com/reus/reflect/ReflectDemo.java
+++ src/main/java/com/reus/reflect/ReflectDemo.java
@@ -13,26 +13,30 @@ import java.util.Date;
  */
 public class ReflectDemo {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         new ReflectDemo().getDemoClass();
     }
 
     /**
-     * 获取java类
+     * 获取java类字节码
      */
-    public void getDemoClass(){
+    public void getDemoClass() {
         //1.类名.class
-        Class<?> cls = Date.class;
-        System.out.println(cls);
+        Class<?> cls1 = Date.class;
+        System.out.println(cls1);
 
         //2.对象.getClass()
-        cls = new Date().getClass();
-        System.out.println(cls);
+        Class<?> cls2 = new Date().getClass();
+        System.out.println(cls2);
 
+        System.out.println(cls1 == cls2);//true
         //3.用静态方法Class.forName("类名")去得到字符串对应的类字节码
         try {
-            cls = Class.forName("java.util.Date");//forName是Class类的静态方法，写类名时一定写出类的全名
-            System.out.println(cls);
+            Class<?> cls3 = Class.forName("java.util.Date");//forName是Class类的静态方法，写类名时一定写出类的全名
+            System.out.println(cls3);
+
+            System.out.println(cls1 == cls3);//true
+            System.out.println(cls2 == cls3);//true
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
